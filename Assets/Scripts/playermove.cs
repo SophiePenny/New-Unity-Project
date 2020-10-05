c94a81d5271b16cb887721affa9c51b709b2e471
Sophie Penny <512894@chrome.escg.ac.uk>
2020-10-05 12:17:06 +0100

    Initial commit

---
+++ Assets/Scripts/playermove.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermove : MonoBehaviour
+{
+
+    
+    // Start is called before the first frame update
+
+
+    public int lives = 1;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xSpeed;
+        float ySpeed;
+
+        if (Input.GetKey("left"))
+        {
+         xSpeed = -1.0f;
+
+         transform.position += new Vector3(xSpeed * Time.deltaTime, 0, 0);
+         
+       
+        }
+
+        if (Input.GetKey("right"))
+        {
+         ySpeed = 1.0f;
+
+         transform.position += new Vector3(ySpeed * Time.deltaTime, 0, 0);
+
+
+        }
+
+        if (Input.GetKey("up"))
+        {
+         ySpeed = 1.0f;
+
+         transform.position += new Vector3(0, ySpeed * Time.deltaTime, 0);
+
+
+        }
+
+        if (Input.GetKey("down"))
+        {
+         ySpeed = -1.0f;
+
+         transform.position += new Vector3(0, ySpeed * Time.deltaTime, 0);
+
+
+        }
+
+        if (Input.GetKey("x"))
+        {
+            lives = 0;
+
+          
+
+
+        }
+
+
+
+    }
+}
